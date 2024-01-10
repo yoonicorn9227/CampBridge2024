a277e8cc026c9028036494c143c7a6c97ac4a72a
KOREAVC <KOREAVC@DESKTOP-LSNB6N2>
2024-01-10 18:39:16 +0900

    20주차2024년 01월 10일 _ CampBridge

---
+++ CampBridge2024/src/main/java/com/java/www/Team2024Application.java
@@ -0,0 +1,13 @@
+package com.java.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team2024Application.class, args);
+	}
+
+}
